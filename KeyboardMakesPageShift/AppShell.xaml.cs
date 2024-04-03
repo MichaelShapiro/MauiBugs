b1597da5486af3d5f6553b788f5263191d630baa
Michael Shapiro <mshapiro@cma.com>
2024-04-03 15:39:20 -0400

    new bug

---
+++ KeyboardMakesPageShift/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KeyboardMakesPageShift
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KeyboardMakesPageShift/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace KeyboardMakesPageShift
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
