1e620e4e90678a3688d5e14191ff426a010cb90a
Michael Shapiro <mshapiro@cma.com>
2025-04-28 14:58:30 -0400

    Snackbar bug

---
+++ SnackbarBug/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Maui.Alerts;
+using CommunityToolkit.Maui.Core;
+
+namespace SnackbarBug
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            string msg = 
+                $"FIRST LINE\n\n\n" +
+
+                $"1111111\n\n\n" +
+                $"2222222\n\n\n" +
+                $"3333333\n\n\n" +
+                $"4444444\n\n\n" +
+                $"5555555\n\n\n" +
+                $"6666666\n\n\n" +
+                $"7777777\n\n\n" +
+                $"8888888\n\n\n" +
+                $"7777777\n\n\n" +
+                $"8888888\n\n\n" +
+                $"9999999\n\n\n" +
+
+
+                $"LAST LINE";
+
+            ISnackbar snackBar = Snackbar.Make(msg, null, "OK", TimeSpan.FromSeconds(10), null, snackbarAnchor);
+            snackBar.Show();
+        }
+    }
+
+}
---
+++ SnackbarBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SnackbarBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
